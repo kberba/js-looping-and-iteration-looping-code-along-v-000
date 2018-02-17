558132c70e8e86670b959b2e51cd844e805f2436
Karen Berba <kberba@gmail.com>
2018-02-17 21:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ function printBadges(badges) {
   return badges;
 }
 
+function coinTrue() {
+  return Math.random() >= 0.5;
+}
+
 function tailsNeverFails() {
-  
+  let counter = 0;
+  while(coinTrue()) {
+    coinTrue();
+    counter++;
+  }  
+  console.log(`You got ${counter} tails in a row!`);
 }

4fd5d182ff3e0780356443cacd99b268be144b28
Karen Berba <kberba@gmail.com>
2018-02-17 21:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function printBadges(badges) {
   return badges;
 }
 
+function coinFlip() {
+  let flip = Math.random();
+  if (flip > 0.5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function tailsNeverFails() {
-  
+  let flipping = coinFlip();
+  while (flipping === true) {
+    coinFlip();
+  }
 }
